8318f49d61a82d91a9ab4efff6d7c68125918170
Patryk Janiak <xmorswinx@gmail.com>
2024-05-25 15:38:06 +0200

    fix(util): static assert false fails compilation

--- include/f2/util/type_to_enum.hpp
+++ include/f2/util/type_to_enum.hpp
@@ -5,6 +5,11 @@
 
 namespace f2 {
 
+  namespace details {
+    template<typename ...>
+    inline constexpr bool always_false = false;
+  }
+
   template<typename T>
   constexpr GLenum type_to_enum() {
     if constexpr (std::is_same_v<T, float>) 
@@ -24,7 +29,7 @@ namespace f2 {
     else if constexpr (std::is_same_v<T, unsigned char>)
       return GL_UNSIGNED_BYTE;
     else
-      static_assert(false, "Unsupported type");
+      static_assert(details::always_false<T>, "Unsupported type");
   }
 
 }
